d9ef6ad33b2f744d144bb9be5ad8df5e24f3010a
anahnarciso <anahnarciso@gmail.com>
2014-09-03 07:48:59 +0100

    Aulas

---
+++ r_programming.R
@@ -0,0 +1,28 @@
+## R Programming
+
+add2 <- function(x, y) {
+    x + y
+}
+
+above10 <- function(x) {
+  use <- x > 10 # vector whose elements are bigger than 10
+  x[use]
+}
+
+above <- function(x, n = 10) {
+  use <- x > n
+  x[use]
+}
+
+columnmean <- function(y, removeNA = TRUE) {
+  nc <- ncol(y)
+  means <- numeric(nc)
+  
+  for(i in 1:nc) {
+    means[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  means
+}
+
+x <- list(a=1:5, b=rnorm(10))
+lapply(x, mean)
